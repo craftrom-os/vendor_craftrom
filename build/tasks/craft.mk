ad43b72f444e81cc5974916906c3436e1cf603c7
melles1991 <melles.android@gmail.com>
2024-06-20 22:15:05 +0300

    derp: Initial changes for CraftRom

--- build/tasks/craft.mk
+++ build/tasks/craft.mk
@@ -1,5 +1,6 @@
 # Copyright (C) 2017 Unlegacy-Android
 # Copyright (C) 2017,2020 The LineageOS Project
+# Copyright (C) 2024 The CrafRom OS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,15 +17,15 @@
 # -----------------------------------------------------------------
 # Lineage OTA update package
 
-LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION).zip
+CRAFT_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
-.PHONY: bacon
-bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
-	$(hide) mv -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
-	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
+.PHONY: craft bacon
+craft: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CRAFT_TARGET_PACKAGE)
+	$(hide) mv -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CRAFT_TARGET_PACKAGE)
+	$(hide) $(SHA256) $(CRAFT_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CRAFT_TARGET_PACKAGE).sha256sum
 	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
 	@echo ""
 	@echo "                                                                " >&2
@@ -34,9 +35,11 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo "                                                                " >&2
 	@echo "------------------------------------------------" >&2
 	@echo " [Timestamp]            : "$(CUSTOM_BUILD_DATE_UTC)
-	@echo " [Size]                 : $(shell du -hs $(LINEAGE_TARGET_PACKAGE) | awk '{print $$1}')"
-	@echo " [Size(in bytes)]       : $(shell wc -c $(LINEAGE_TARGET_PACKAGE) | awk '{print $$1}')"
-	@echo " [Package]              : "$(LINEAGE_TARGET_PACKAGE)"               "
-	@echo " [SHA256]               : `sha256sum $(LINEAGE_TARGET_PACKAGE) | cut -d ' ' -f 1` "
+	@echo " [Size]                 : $(shell du -hs $(CRAFT_TARGET_PACKAGE) | awk '{print $$1}')"
+	@echo " [Size(in bytes)]       : $(shell wc -c $(CRAFT_TARGET_PACKAGE) | awk '{print $$1}')"
+	@echo " [Package]              : "$(CRAFT_TARGET_PACKAGE)"               "
+	@echo " [SHA256]               : `sha256sum $(CRAFT_TARGET_PACKAGE) | cut -d ' ' -f 1` "
 	@echo "------------------------------------------------" >&2
 	@echo ""
+
+bacon: craft	
